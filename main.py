53b5f8abcfd08fa4cd97c4cddba422de05dff1f7
AndrewZimin <149013980+AndrewZimin@users.noreply.github.com>
2023-10-31 22:45:44 +0200

    init setup and pycharm integration checkup

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# test
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question_record in question_data:
+    question_bank.append(Question(question_record["text"], question_record["answer"]))
+
+print(question_bank)
