980969969a4b30fa867f5534a3128b7e29a6ea7b
AndrewZimin <149013980+AndrewZimin@users.noreply.github.com>
2023-10-31 23:23:06 +0200

    part 4 changes, adding quiz questions functionality

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 
 for question_record in question_data:
     question_bank.append(Question(question_record["text"], question_record["answer"]))
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+
+quiz.next_question()
---
+++ quiz_brain.py
@@ -0,0 +1,11 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        question_item = self.question_list
+        question_text = question_item[self.question_number].text
+        print("Please type your answer to the question")
+        question_answer = input(f"{question_text}: ")
+
